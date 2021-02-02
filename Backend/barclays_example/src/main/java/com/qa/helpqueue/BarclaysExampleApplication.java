8bbe00907cf8697c68be1a81881bcc7e0cb2b3cf
reece-elder <reece.elder@gmail.com>
2021-02-02 14:49:48 +0000

    Basic Backend Logic completed

---
+++ Backend/barclays_example/src/main/java/com/qa/helpqueue/BarclaysExampleApplication.java
@@ -0,0 +1,13 @@
+package com.qa.helpqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarclaysExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarclaysExampleApplication.class, args);
+	}
+
+}
