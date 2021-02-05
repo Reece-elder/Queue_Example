6ad495d57ca4e8c2fa112e4b6d8d926c8257bff6
reece-elder <reece.elder@qa.com>
2021-02-05 12:20:29 +0000

    RE: Modified

--- DevOps/Jenkins/build_backend.sh
+++ DevOps/Jenkins/build_backend.sh
@@ -9,18 +9,7 @@ echo ${testvm_ip}
 echo "database credentials exported"
 
 ssh ubuntu@${testvm_ip} <<EOF
-echo "I'm Sshed in"
-cd ~
-/home/ubuntu/databasecredentials.sh
-
-if [ ! -d /home/jenkins/.jenkins/workspace/queue_project/Queue_Example ]; then
-   git clone https://github.com/Reece-elder/Queue_Example.git -b dev
-else 
-   rm -rf Queue_Example
-   git clone https://github.com/Reece-elder/Queue_Example.git -b dev
-fi
-
-cd ~/Group-3-Final-Project/Spring-HelpQueue
+cd ~/.jenkins/workspace/queue_project/Backend/barclays_example
 mvn clean package | grep 'BUILD SUCCESS'
 
 EOF
