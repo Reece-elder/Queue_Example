4e1848888eae0e47c1eb476f884f554f0814b556
reece-elder <reece.elder@qa.com>
2021-02-05 10:25:53 +0000

    Modified Script

--- DevOps/Jenkins/clone_repo.sh
+++ DevOps/Jenkins/clone_repo.sh
@@ -1,14 +1,11 @@
  #! /bin/bash
-echo "===== Location ====="
-pwd
-echo "===== Location ====="
+ 
 sleep 2
-cd /var/lib/jenkins/workspace/queue_project
-if [ ! -d /var/lib/jenkins/workspace/queue_project/Queue_Example ]; then
-   git clone https://github.com/Reece-elder/Queue_Example.git -b Dev
+if [ ! -d /home/jenkins/.jenkins/workspace/queue_project/Queue_Example ]; then
+   git clone https://github.com/Reece-elder/Queue_Example.git -b dev
 else 
    rm -rf Queue_Example
-   git clone https://github.com/Reece-elder/Queue_Example.git -b Dev
+   git clone https://github.com/Reece-elder/Queue_Example.git -b dev
 fi
 
 echo "*hacker voice* I'm in."
